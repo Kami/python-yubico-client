3fa0a5bf61f0ce7e036f0ca4b83d2bccc2941228
Tomaž Muraus <kami@k5-storitve.net>
2010-05-09 09:11:43 +0200

    Moved exceptions to a separate module and added a InvalidClientIdError exception

---
+++ yubico_exceptions.py
@@ -0,0 +1,26 @@
+class YubicoError(Exception):
+	""" Base class for Yubico related exceptions. """
+	pass
+
+class StatusCodeError(YubicoError):
+	def __init__(self, status_code):
+		self.status_code = status_code
+		
+	def __str__(self):
+		return 'Yubico server returned the following status code: %s' % (self.status_code)
+	
+class InvalidClientIdError(YubicoError):
+	def __init__(self, client_id):
+		self.client_id = client_id
+		
+	def __str__(self):
+		return 'The client with ID %s does not exist' % (self.client_id)
+	
+class SignatureVerificationError(YubicoError):
+	def __init__(self, generated_signature, response_signature):
+		self.generated_signature = generated_signature
+		self.response_signature = response_signature
+		
+	def __str__(self):
+		return repr('Server response message signature verification failed (expected %s, got %s)' \
+				% (self.generated_signature, self.response_signature))
