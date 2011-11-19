8142f4b65ecc5042403602c6018894a28029d509
Tomaz Muraus <tomaz@tomaz.me>
2011-11-19 18:25:33 +0100

    Update tests.

--- tests/test_yubico.py
+++ tests/test_yubico.py
@@ -9,7 +9,18 @@ from yubico.yubico_exceptions import StatusCodeError, InvalidClientIdError
 from yubico.yubico_exceptions import SignatureVerificationError
 
 
-class TestYubico(unittest.TestCase):
+class TestOTPClass(unittest.TestCase):
+    def test_otp_class(self):
+        otp1 = OTP('tlerefhcvijlngibueiiuhkeibbcbecehvjiklltnbbl')
+        otp2 = OTP('jjjjjjjjnhe.ngcgjeiuujjjdtgihjuecyixinxunkhj',
+                  translate_otp=True)
+
+        self.assertEqual(otp1.device_id, 'tlerefhcvijl')
+        self.assertEqual(otp2.otp,
+                         'ccccccccljdeluiucdgffccchkugjcfditgbglbflvjc')
+
+
+class TestYubicoVerifySingle(unittest.TestCase):
     def setUp(self):
         yubico.API_URLS = (
                 '127.0.0.1:8888/wsapi/2.0/verify',
@@ -21,24 +32,30 @@ class TestYubico(unittest.TestCase):
         self.client_verify_sig = yubico.Yubico('1234', 'secret123456',
                                                use_https=False)
 
-    def test_otp_class(self):
-        otp = OTP('tlerefhcvijlngibueiiuhkeibbcbecehvjiklltnbbl')
-        self.assertEqual(otp.device_id, 'tlerefhcvijl')
+    def test_replayed_otp(self):
+        self._set_mock_action('REPLAYED_OTP')
+
+        try:
+            self.client_no_verify_sig.verify('bad')
+        except StatusCodeError, e:
+            self.assertEqual(e.status_code, 'REPLAYED_OTP')
 
     def test_verify_bad_status_codes(self):
-        for status in yubico.BAD_STATUS_CODES:
+        for status in (set(yubico.BAD_STATUS_CODES) - set(['REPLAYED_OTP'])):
             self._set_mock_action(status)
+
             try:
                 self.client_no_verify_sig.verify('bad')
-            except StatusCodeError, e:
-                self.assertEqual(e.status_code, status)
-            else:
-                self.fail('Exception was not thrown')
+            except Exception, e:
+                self.assertEqual(str(e), 'NO_VALID_ANSWERS')
 
     def test_verify_local_timeout(self):
         self._set_mock_action('timeout')
 
-        self.assertEqual(self.client_no_verify_sig.verify('bad'), None)
+        try:
+            self.client_no_verify_sig.verify('bad')
+        except Exception, e:
+            self.assertEqual(str(e), 'NO_VALID_ANSWERS')
 
     def test_verify_invalid_signature(self):
         self._set_mock_action('no_signature_ok')
@@ -50,7 +67,7 @@ class TestYubico(unittest.TestCase):
         else:
             self.fail('Exception was not thrown')
 
-    def test_verify_NO_SUCH_CLIENT(self):
+    def test_verify_no_such_client(self):
         self._set_mock_action('no_such_client')
 
         try:
