92ac6d6ddcc8569b7ee5331aa22cc467c3a5b243
Tomaz Muraus <tomaz@tomaz.me>
2015-10-02 22:52:15 +0200

    Use version specifier which works with older versions of setuptools.

--- setup.py
+++ setup.py
@@ -97,7 +97,7 @@ setup(
     packages=['yubico_client'],
     provides=['yubico_client'],
     install_requires=[
-        'requests~=2.7',
+        'requests>=2.7,<3.0',
     ],
     cmdclass={
         'test': TestCommand,
