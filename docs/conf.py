a8879947b0d0a4a6cc72b556a49d0eb62d71a078
Tomaz Muraus <tomaz@tomaz.me>
2019-11-18 13:53:42 +0100

    Update copyright date.

--- docs/conf.py
+++ docs/conf.py
@@ -33,7 +33,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Yubico Client'
-copyright = u'2013, Tomaz Muraus'
+copyright = u'2013-2019, Tomaz Muraus'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
